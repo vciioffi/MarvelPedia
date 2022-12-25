80c446ba7ea22f2a58c4079d541f73ace87b00d3
vciioffi <vciioffi@gmail.com>
2022-12-25 23:58:04 +0100

    feature: implementing HeroesComicsResponseDto.kt

--- app/src/main/java/com/example/marvelpedia/heroes/data/model/HeroesComicsResponseDto.kt
+++ app/src/main/java/com/example/marvelpedia/heroes/data/model/HeroesComicsResponseDto.kt
@@ -2,7 +2,7 @@ package com.example.marvelpedia.heroes.data.model
 import com.google.gson.annotations.SerializedName
 
 
-data class HeroesResponseDto(
+data class HeroesComicsResponseDto(
     @SerializedName("attributionHTML")
     val attributionHTML: String,
     @SerializedName("attributionText")
@@ -19,7 +19,7 @@ data class HeroesResponseDto(
     val status: String
 )
 
-data class Data(
+ data class DataComics(
     @SerializedName("count")
     val count: Int,
     @SerializedName("limit")
@@ -27,37 +27,73 @@ data class Data(
     @SerializedName("offset")
     val offset: Int,
     @SerializedName("results")
-    val results: List<HeroesDto>,
+    val results: List<Result>,
     @SerializedName("total")
     val total: Int
 )
 
-data class HeroesDto(
-    @SerializedName("comics")
-    val comics: Comics,
+data class Result(
+    @SerializedName("characters")
+    val characters: Characters,
+    @SerializedName("collectedIssues")
+    val collectedIssues: List<Any>,
+    @SerializedName("collections")
+    val collections: List<Any>,
+    @SerializedName("creators")
+    val creators: Creators,
+    @SerializedName("dates")
+    val dates: List<Date>,
     @SerializedName("description")
     val description: String,
+    @SerializedName("diamondCode")
+    val diamondCode: String,
+    @SerializedName("digitalId")
+    val digitalId: Int,
+    @SerializedName("ean")
+    val ean: String,
     @SerializedName("events")
     val events: Events,
+    @SerializedName("format")
+    val format: String,
     @SerializedName("id")
     val id: Int,
+    @SerializedName("images")
+    val images: List<Image>,
+    @SerializedName("isbn")
+    val isbn: String,
+    @SerializedName("issn")
+    val issn: String,
+    @SerializedName("issueNumber")
+    val issueNumber: Int,
     @SerializedName("modified")
     val modified: String,
-    @SerializedName("name")
-    val name: String,
+    @SerializedName("pageCount")
+    val pageCount: Int,
+    @SerializedName("prices")
+    val prices: List<Price>,
     @SerializedName("resourceURI")
     val resourceURI: String,
     @SerializedName("series")
     val series: Series,
     @SerializedName("stories")
     val stories: Stories,
+    @SerializedName("textObjects")
+    val textObjects: List<TextObject>,
     @SerializedName("thumbnail")
     val thumbnail: Thumbnail,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("upc")
+    val upc: String,
     @SerializedName("urls")
-    val urls: List<Url>
+    val urls: List<Url>,
+    @SerializedName("variantDescription")
+    val variantDescription: String,
+    @SerializedName("variants")
+    val variants: List<Variant>
 )
 
-data class Comics(
+data class Characters(
     @SerializedName("available")
     val available: Int,
     @SerializedName("collectionURI")
@@ -66,6 +102,24 @@ data class Comics(
     val items: List<Item>,
     @SerializedName("returned")
     val returned: Int
+)
+
+data class Creators(
+    @SerializedName("available")
+    val available: Int,
+    @SerializedName("collectionURI")
+    val collectionURI: String,
+    @SerializedName("items")
+    val items: List<ItemX>,
+    @SerializedName("returned")
+    val returned: Int
+)
+
+data class Date(
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("type")
+    val type: String
 )
 
 data class Events(
@@ -79,15 +133,25 @@ data class Events(
     val returned: Int
 )
 
+data class Image(
+    @SerializedName("extension")
+    val extension: String,
+    @SerializedName("path")
+    val path: String
+)
+
+data class Price(
+    @SerializedName("price")
+    val price: Double,
+    @SerializedName("type")
+    val type: String
+)
+
 data class Series(
-    @SerializedName("available")
-    val available: Int,
-    @SerializedName("collectionURI")
-    val collectionURI: String,
-    @SerializedName("items")
-    val items: List<Item>,
-    @SerializedName("returned")
-    val returned: Int
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("resourceURI")
+    val resourceURI: String
 )
 
 data class Stories(
@@ -99,6 +163,15 @@ data class Stories(
     val items: List<ItemXXX>,
     @SerializedName("returned")
     val returned: Int
+)
+
+data class TextObject(
+    @SerializedName("language")
+    val language: String,
+    @SerializedName("text")
+    val text: String,
+    @SerializedName("type")
+    val type: String
 )
 
 data class Thumbnail(
@@ -115,11 +188,27 @@ data class Url(
     val url: String
 )
 
+data class Variant(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("resourceURI")
+    val resourceURI: String
+)
+
 data class Item(
     @SerializedName("name")
     val name: String,
     @SerializedName("resourceURI")
     val resourceURI: String
+)
+
+data class ItemX(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("resourceURI")
+    val resourceURI: String,
+    @SerializedName("role")
+    val role: String
 )
 
 data class ItemXXX(
